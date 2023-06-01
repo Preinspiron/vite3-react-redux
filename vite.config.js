899ab2fbe0f5d31ca53f46955a1035af12220996
V_Borysenko <preinspiron@gmail.com>
2023-06-01 22:11:14 +0200

    init 2

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve} from 'path'
-import path from 'path'
+// import { resolve} from 'path'
+// import path from 'path'
 
-import url from 'url'
+// import url from 'url'
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/vite3-react-redux/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "index.html"),
         
-      },
-    },
-  },
+  //     },
+  //   },
+  // },
 })
