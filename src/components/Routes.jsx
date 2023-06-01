079830e2b5a0fb8b69610059541cde88e1ee988b
V_Borysenko <preinspiron@gmail.com>
2023-06-01 21:07:28 +0200

    init 2

---
+++ src/components/Routes.jsx
@@ -0,0 +1,17 @@
+
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Tweets from '../pages/Tweets';
+
+
+function Routing() {
+    return (
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/tweets/*" element={<Tweets />} />
+        
+      </Routes>
+    );
+  }
+  
+  export default Routing;
