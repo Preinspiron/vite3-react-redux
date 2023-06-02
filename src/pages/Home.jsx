1ce5a85949a209f86165dc18b915f4b02e4c16c6
Olena Korenieva <olena.korenieva@gmail.com>
2023-06-02 21:47:55 +0200

    changed logic for load more button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,108 +1,28 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import Tweet from '@/components/Tweet/Tweet';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import s from './Home.module.scss';
 
-import { useGetUsersQuery, usePutUserMutation } from '@/api/store';
-
-const tempArr = [
-  {
-    createdAt: '2023-06-01T02:37:47.927Z',
-    user: 'Dr. Ralph Feest',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/560.jpg',
-    followers: 89287,
-    tweets: 22920,
-    id: '1',
-  },
-  {
-    createdAt: '2023-06-01T04:42:18.132Z',
-    user: 'Constance Ortiz',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/882.jpg',
-    followers: 95617,
-    tweets: 73154,
-    id: '2',
-  },
-  {
-    createdAt: '2023-06-01T06:40:39.956Z',
-    user: 'Mrs. Holly Hyatt',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1033.jpg',
-    followers: 80975,
-    tweets: 77144,
-    id: '3',
-  },
-  {
-    createdAt: '2023-06-01T02:37:47.927Z',
-    user: 'Dr. Ralph Feest',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/560.jpg',
-    followers: 89287,
-    tweets: 22920,
-    id: '4',
-  },
-  {
-    createdAt: '2023-06-01T04:42:18.132Z',
-    user: 'Constance Ortiz',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/882.jpg',
-    followers: 95617,
-    tweets: 73154,
-    id: '5',
-  },
-  {
-    createdAt: '2023-06-01T06:40:39.956Z',
-    user: 'Mrs. Holly Hyatt',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1033.jpg',
-    followers: 80975,
-    tweets: 77144,
-    id: '6',
-  },
-  {
-    createdAt: '2023-06-01T06:40:39.956Z',
-    user: 'Mrs. Holly Hyatt',
-    avatar:
-      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1033.jpg',
-    followers: 80975,
-    tweets: 77144,
-    id: '7',
-  },
-];
+import { useGetUsersQuery } from '@/api/store';
 
 const Home = () => {
-  const { data, isFetching } = useGetUsersQuery();
-  console.log(data);
-  const [page, setPage] = useState(1);
-  const [usersArr, setUsersArr] = useState([]);
+  const { data = [] } = useGetUsersQuery();
 
-  const maxPage = Math.ceil(tempArr.length / 3);
+  const cardsInRow = 3;
+  const [nextNumber, setNextNumber] = useState(cardsInRow);
 
-  useEffect(() => {
-    getPaginatedObjects();
-  }, [page]);
-
-  const getPaginatedObjects = () => {
-    const startIndex = (page - 1) * 3;
-    const endIndex = startIndex + 3;
-    const sliceTempArr = tempArr.slice(startIndex, endIndex);
-    setUsersArr((prev) => [...prev, ...sliceTempArr]);
-  };
-
-  // при перезагрузке страницы не меняется page
-  const incrementPage = () => {
-    setPage((prev) => prev + 1);
+  const handleLoadMore = () => {
+    setNextNumber(nextNumber + cardsInRow);
   };
 
   return (
     <div>
-      <ul>
-        {usersArr.map((item) => (
+      <ul className={s.homePage}>
+        {data.slice(0, nextNumber).map((item) => (
           <Tweet key={item.id} {...item} />
         ))}
       </ul>
-      {maxPage !== page && (
-        <button type="button" onClick={incrementPage}>
+      {nextNumber < data.length && (
+        <button type="button" className={s.loadBtn} onClick={handleLoadMore}>
           Load More
         </button>
       )}
@@ -110,5 +30,4 @@ const Home = () => {
   );
 };
 
-export default Home;
 export default Home;
