9263b1fe536c240370c9a8a8f334fd811b3a6484
Dubyshkina <ek.kalyazina@gmail.com>
2023-06-02 10:33:42 +0300

    did structure

---
+++ src/components/Tweet/Tweet.jsx
@@ -0,0 +1,13 @@
+import Tweet from "../Tweet/Tweet";
+
+const Tweets = () => {
+   
+    // селектор с массивом юзеров 
+    return <ul>
+        {/* или меп */}
+        {/* {users.map(user => <Tweet key={user.id} id={user.id} avatar={user.avatar} followers={user.followers} tweets={user.tweets} />)} */}
+    {/* или фильтр по 1 условию */}
+    {/* или фильтр по 2 условию */}
+    </ul>
+}
+export default Tweets;
