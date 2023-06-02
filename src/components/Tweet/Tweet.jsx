25f472cc5274fbb21b43c7ed47c1d5b0f46a5707
V_Borysenko <preinspiron@gmail.com>
2023-06-02 10:46:12 +0200

    comments tweets

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,15 +1,15 @@
 
-import s from './Tweet.module.scss';
+// import s from './Tweet.module.scss';
 
 
-const Tweet = ({id, avatar, followers, tweets}) => {
+const Tweet = () => {
     // притянуть селектор, который проверяет флаг из ектив и передает по условию класс в кнопку ниже
-    return <li id={id}>
+    // return <li id={id}>
        
-        <img loading="lazy" src={avatar} alt="avatar" className={s.avatar}/>
-        <p className={s.tweets}>`${tweets} Tweets`</p>
-        <p className={s.followers}>`${followers} Followers`</p>
-        <button type="button" className={s.button}>text by class</button>
-        </li>
+    //     <img loading="lazy" src={avatar} alt="avatar" className={s.avatar}/>
+    //     <p className={s.tweets}>`${tweets} Tweets`</p>
+    //     <p className={s.followers}>`${followers} Followers`</p>
+    //     <button type="button" className={s.button}>text by class</button>
+    //     </li>
 }
 export default Tweet;
