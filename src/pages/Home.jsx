3088661241de499a2131c7a5dcc34f4c84d4aefd
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-06-02 19:17:58 +0300

    cards (#25)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,42 @@
-import Tweet from '@/components/Tweet/Tweet'
-const tempArr =[ {
-    "createdAt": "2023-06-01T02:37:47.927Z",  "user": "Dr. Ralph Feest",
-    "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/560.jpg",  "followers": 89287,
-    "tweets": 22920,  "id": "1"
-   }, {
-    "createdAt": "2023-06-01T04:42:18.132Z",  "user": "Constance Ortiz",
-    "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/882.jpg",  "followers": 95617,
-    "tweets": 73154,  "id": "2"
-   }, {
-    "createdAt": "2023-06-01T06:40:39.956Z",  "user": "Mrs. Holly Hyatt",
-    "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1033.jpg",  "followers": 80975,
-    "tweets": 77144,  "id": "3"
-   }]
+import Tweet from '@/components/Tweet/Tweet';
+import s from './Home.module.scss';
 
-const Home = () => <ul>
-{tempArr.map(item=><Tweet key={item.id} {...item}/>)}
-</ul>
+const tempArr = [
+  {
+    createdAt: '2023-06-01T02:37:47.927Z',
+    user: 'Dr. Ralph Feest',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/560.jpg',
+    followers: 89287,
+    tweets: 22920,
+    id: '1',
+  },
+  {
+    createdAt: '2023-06-01T04:42:18.132Z',
+    user: 'Constance Ortiz',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/882.jpg',
+    followers: 95617,
+    tweets: 73154,
+    id: '2',
+  },
+  {
+    createdAt: '2023-06-01T06:40:39.956Z',
+    user: 'Mrs. Holly Hyatt',
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1033.jpg',
+    followers: 80975,
+    tweets: 77144,
+    id: '3',
+  },
+];
 
+const Home = () => (
+  <ul className={s.homePage}>
+    {tempArr.map((item) => (
+      <Tweet key={item.id} {...item} />
+    ))}
+  </ul>
+);
 
-export default Home
-
+export default Home;
