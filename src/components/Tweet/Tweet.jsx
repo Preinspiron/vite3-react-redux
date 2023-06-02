7faef2db911c8477cec3a70f9a03cbfb7881a829
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-06-02 23:24:03 +0300

    buttons (#30)
    Co-authored-by: Volodymyr <68303955+Preinspiron@users.noreply.github.com>

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import s from './Tweet.module.scss';
 import PropTypes from 'prop-types';
 
@@ -8,16 +9,29 @@ const Tweet = ({
   followers = null,
   tweets = null,
 }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const handleButtonClick = () => {
+    setIsFollowing(!isFollowing); // Toggle the value of isFollowing
+    followers = isFollowing ? followers - 1 : followers + 1; // Update the followers count
+  };
+  const buttonStyle = {
+    backgroundColor: isFollowing ? '#5CD3A8' : '#EBD8FF',
+  };
+
+  // притянуть селектор, который проверяет флаг из ектив и передает по условию класс в кнопку ниже
 
   return (
     <li id={id} className={s.wrapper}>
       <img loading="lazy" src={avatar} alt="avatar" className={s.avatar} />
       <p className={s.tweets}>{tweets} Tweets</p>
       <p className={s.followers}>{followers} Followers</p>
-      <p className={s.tweets}>{tweets} Tweets</p>
-      <p className={s.followers}>{followers} Followers</p>
-      <button type="button" className={s.button}>
-        FOLLOW
+      <button
+        type="button"
+        className={s.button}
+        onClick={handleButtonClick}
+        style={buttonStyle}
+      >
+        {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
       </button>
     </li>
   );
