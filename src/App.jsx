36d63a65e1bf475398f619196310e31ee6bc3b3e
V_Borysenko <preinspiron@gmail.com>
2023-06-02 12:55:27 +0200

    congifs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Routing from './components/Routing'
 import './App.css'
-// import {Link} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
