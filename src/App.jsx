8a613ce7a5ece4c77f2dd4f2e4cbc83d05f3259b
Helen <51207721+ElenaKorenieva@users.noreply.github.com>
2023-06-02 21:53:12 +0200

    Load more (#28)
    * add load more button logic
    
    * cleaned console.log
    
    * fixed load more button logic
    
    * changed logic for load more button

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth="1280px">
       <Routing />
     </Container>
   );
