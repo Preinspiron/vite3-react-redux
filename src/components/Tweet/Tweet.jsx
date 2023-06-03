cbd1e82c7f1f6d9a2b1445411855410fb8fca155
Volodymyr <68303955+Preinspiron@users.noreply.github.com>
2023-06-03 13:01:55 +0200

    Borysenko (#35)
    * layout
    
    * congifs
    
    * tweets
    
    * header styles
    
    * store
    
    * 123
    
    * useGet
    
    * 123
    
    * usePut
    
    * checker
    
    * checker
    
    * checker
    
    * checker

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import s from './Tweet.module.scss';
 import { usePutUserMutation } from '@/api/store';
+
+
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFollow, removeFollow } from '@/api/slice';
@@ -11,22 +13,25 @@ const Tweet = ({
   followers = null,
   tweets = null,
 }) => {
-  const [isFollowing, setIsFollowing] = useState(false);
-  const [putUser, { isSuccess }] = usePutUserMutation();
-  const dispatch = useDispatch();
-  const selectFollowings = useSelector((state) => state.followers.follows);
+
+
+  const selectFollowings = useSelector(state => state.followers.follows)
+    const checker = selectFollowings.some(item => item?.id === id) 
+  const [isFollowing, setIsFollowing] = useState(checker);
+  const [putUser, {isSuccess}] = usePutUserMutation()
+  const dispatch = useDispatch()
+  
 
   const handleButtonClick = (id, followers) => {
-    !isFollowing && dispatch(setFollow(id));
-    isFollowing && dispatch(removeFollow(id));
-    putUser({
-      id,
-      followers: isFollowing ? followers - 1 : followers + 1,
-    }).unwrap();
+   
+
+    !isFollowing && dispatch(setFollow(id))
+    isFollowing && dispatch(removeFollow(id))
+    putUser({id, followers: isFollowing ? followers-1: followers+1}).unwrap()
     setIsFollowing(!isFollowing);
   };
-  const checker = selectFollowings.some((item) => item?.id === id);
-  checker;
+
+
 
   const buttonStyle = {
     backgroundColor: checker ? '#5CD3A8' : '#EBD8FF',
@@ -35,26 +40,30 @@ const Tweet = ({
   // притянуть селектор, который проверяет флаг из ектив и передает по условию класс в кнопку ниже
 
   return (
-    <li id={id} className={s.wrapper}>
+
+    <li id={id} className={s.wrapper} >
       <img loading="lazy" src={avatar} alt="avatar" className={s.avatar} />
       <p className={s.tweets}>{tweets} Tweets</p>
       <p className={s.followers}>{followers} Followers</p>
       <button
         type="button"
         className={s.button}
-        onClick={() => handleButtonClick(id, followers)}
+        onClick={()=>handleButtonClick(id, followers)}
         style={buttonStyle}
       >
         {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
       </button>
     </li>
-  );
-};
+  )
+}
 
-export default Tweet;
+      export default Tweet;
+    
 Tweet.propTypes = {
   id: PropTypes.string,
   avatar: PropTypes.string,
   followers: PropTypes.number,
   tweets: PropTypes.number,
+  
+  
 };
