530079844a4874dc7dbd438b702759db1437728c
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-06-03 19:51:49 +0300

    Loader

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -1,42 +1,63 @@
-import { useState } from "react";
-import Tweet from "../components/Tweet/Tweet";
-import Filter from "../components/Filter/Filter";
-import { useGetUsersQuery} from '@/api/store';
+import { useState } from 'react';
+import Tweet from '../components/Tweet/Tweet';
+import Filter from '../components/Filter/Filter';
+import { useGetUsersQuery } from '@/api/store';
 
-
-const idies = {     
-        id:['2', '3', '6']
-}
+const idies = {
+  id: ['2', '3', '6'],
+};
 const Tweets = () => {
-
-    const {data=[]}  = useGetUsersQuery();
-    console.log(data)
-    const [filter, setFilter] = useState('');
-
-
+  const { data = [] } = useGetUsersQuery();
+  console.log(data);
+  const [filter, setFilter] = useState('');
 
   const change = (data) => {
-   setFilter(data);
-  }
+    setFilter(data);
+  };
 
-return <div className="container">
-      <Filter 
-      change={change}
-      />
-     <div className="tweets_container">
-    <ul className="tweets_list">
-
-{filter === 'followings' &&
-
-data.filter(user => idies.id.includes(user.id)).map(user => <Tweet key={user.id} id={user.id} avatar={user.avatar} followers={user.followers} tweets={user.tweets} />) }
-{filter === 'follow' &&
-data.filter(user => !idies.id.includes(user.id)).map(user => <Tweet key={user.id} id={user.id} avatar={user.avatar} followers={user.followers} tweets={user.tweets} />) }
-{filter === 'all' && data.map(user => <Tweet key={user.id} id={user.id} avatar={user.avatar} followers={user.followers} tweets={user.tweets} />)
-}
-</ul>
-</div>
-</div>
-}
-
+  return (
+    <div className="container">
+      <Filter change={change} />
+      <div className="tweets_container">
+        <ul className="tweets_list">
+          {filter === 'followings' &&
+            data
+              .filter((user) => idies.id.includes(user.id))
+              .map((user) => (
+                <Tweet
+                  key={user.id}
+                  id={user.id}
+                  avatar={user.avatar}
+                  followers={user.followers}
+                  tweets={user.tweets}
+                />
+              ))}
+          {filter === 'follow' &&
+            data
+              .filter((user) => !idies.id.includes(user.id))
+              .map((user) => (
+                <Tweet
+                  key={user.id}
+                  id={user.id}
+                  avatar={user.avatar}
+                  followers={user.followers}
+                  tweets={user.tweets}
+                />
+              ))}
+          {filter === 'all' &&
+            data.map((user) => (
+              <Tweet
+                key={user.id}
+                id={user.id}
+                avatar={user.avatar}
+                followers={user.followers}
+                tweets={user.tweets}
+              />
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Tweets;
